ac7de034c9d27bd43b848165c4174bd5a41c5ff5
zhaowei <zhaowei@yunniao.me>
2020-08-21 18:07:24 +0800

    vant全局引入

---
+++ src/components/SelfPopup.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="SelfPopup">
+    <van-popup
+      v-model="showPopup"
+      :position="position"
+      :style="{
+        width: '80%',
+        height: '100vh'
+      }"
+    >
+      <van-form
+        style="position: relative;"
+        @submit="onSubmit"
+      >
+        <slot />
+      </van-form>
+
+      <div class="bottom">
+        <van-button block type="primary" size="normal">
+          小型按钮
+        </van-button>
+        <van-button block type="primary" size="normal">
+          小型按钮
+        </van-button>
+      </div>
+    </van-popup>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelfPopup',
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    position: {
+      type: String,
+      default: 'right'
+    }
+  },
+  computed: {
+    showPopup: {
+      get() {
+        return this.show
+      },
+      set(val) {
+        this.$emit('update:show', val)
+      }
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values);
+    },
+    onReset(values) {
+      console.log(values);
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.SelfPopup .bottom{
+  position: sticky;
+  bottom: 0;
+  padding: 10px 18px;
+  display: flex;
+  background-color: #fff;
+}
+.SelfPopup .van-button + .van-button{
+  margin-left: 20px;
+}
+</style>
+<style scoped>
+.SelfPopup >>> .van-popup{
+  box-sizing: border-box;
+}
+</style>
---
+++ src/views/demo.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="Demo">
+    <van-cell is-link @click="showPopup">
+      展示弹出层
+    </van-cell>
+    <SelfPopup ref="container" :show.sync="show">
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{required: true, message: '请填写用户名'}]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{required: true, message: '请填写密码'}]"
+      />
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{required: true, message: '请填写用户名'}]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{required: true, message: '请填写密码'}]"
+      />
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{required: true, message: '请填写用户名'}]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{required: true, message: '请填写密码'}]"
+      />
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{required: true, message: '请填写用户名'}]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{required: true, message: '请填写密码'}]"
+      />
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{required: true, message: '请填写用户名'}]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{required: true, message: '请填写密码'}]"
+      />
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{required: true, message: '请填写用户名'}]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{required: true, message: '请填写密码'}]"
+      />
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{required: true, message: '请填写用户名'}]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{required: true, message: '请填写密码'}]"
+      />
+    </SelfPopup>
+  </div>
+</template>
+
+<script>
+import SelfPopup from '@/components/SelfPopup';
+export default {
+  components: {
+    SelfPopup
+  },
+  data() {
+    return {
+      show: false,
+      username: '',
+      password: ''
+    };
+  },
+
+  methods: {
+    showPopup() {
+      this.show = true;
+    }
+  }
+};
+</script>
+
+<style>
+.sticky{
+  position: sticky;
+  bottom: 20px;
+}
+</style>
