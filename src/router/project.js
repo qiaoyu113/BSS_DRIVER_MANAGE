2d927d04fa52f0923ef533d0b9081a4007670a82
yijie <839108892@qq.com>
2020-08-28 17:44:43 +0800

    1.项目管理

---
+++ src/router/project.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    path: '/project',
+    name: 'Project',
+    component: resolve => require(['@/views/project/List'], resolve),
+    meta: {
+      title: '项目管理',
+      keepAlive: false
+    }
+  },
+  {
+    path: '/projectSearch',
+    name: 'ProjectSearch',
+    component: resolve => require(['@/views/project/Search'], resolve),
+    meta: {
+      title: '项目搜索',
+      keepAlive: false
+    }
+  },
+  {
+    path: '/projectDetail',
+    name: 'ProjectDetail',
+    component: resolve => require(['@/views/project/Detail'], resolve),
+    meta: {
+      title: '项目详情',
+      keepAlive: false
+    }
+  }
+]
