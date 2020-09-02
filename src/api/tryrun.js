0629661a85102133e8b2497f4c0d1277c30e5bc1
zhaowei <zhaowei@yunniao.me>
2020-09-02 18:19:07 +0800

    创建试跑联调接口

--- src/api/tryrun.js
+++ src/api/tryrun.js
@@ -1,9 +1,49 @@
 import request from '@/utils/request'
-let prefix = '/mock/74'
+let prefix = '/api'
 // 创建试跑
 export function CreateLntentionRun(data) {
   return request({
+    url: `${prefix}/waybill/v2/runtest/creatIntentionRun`,
+    method: 'post',
+    data
+  })
+}
+// 选择线路
+export function CreateLntentionRun2(data) {
+  return request({
     url: `${prefix}/v2/runtest/creatIntentionRun`,
+    method: 'post',
+    data
+  })
+}
+// 选择司机
+export function GetDriverList(data) {
+  return request({
+    url: `${prefix}/carrier_center/v2/driver/getDriverList`,
+    method: 'post',
+    data
+  })
+}
+// 选择到仓联系人
+export function GetPersonInfo(params) {
+  return request({
+    url: `${prefix}/line/v2/line/findPersonInfo`,
+    method: 'get',
+    params
+  })
+}
+// 试跑
+export function TryRun(data) {
+  return request({
+    url: `${prefix}/waybill/v2/runtest/tryRun`,
+    method: 'post',
+    data
+  })
+}
+// 跟车
+export function FollowCar(data) {
+  return request({
+    url: `${prefix}/waybill/v2/runtest/followCar `,
     method: 'post',
     data
   })
@@ -12,6 +52,14 @@ export function CreateLntentionRun(data) {
 export function GetRunTestInfoList(data) {
   return request({
     url: `${prefix}/v2/runtest/getRunTestInfoList`,
+    method: 'post',
+    data
+  })
+}
+// 试跑详情
+export function GetDetails(data) {
+  return request({
+    url: `${prefix}/v2/runtest/runDetial`,
     method: 'post',
     data
   })
