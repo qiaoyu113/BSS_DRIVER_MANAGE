9cc5ad0296f8afb8c1dc493d0b05cba17cf3c05d
qiaoyu <qiaoyu@yunniao.me>
2020-09-08 12:55:32 +0800

    更新 request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { Toast } from 'vant'
 // import store from '@/store'
-// import { getToken } from '@/utils/auth'
+import { getToken } from '@/utils/auth'
 import JsCookie from 'js-cookie'
 
 // let url = 'http://firmiana-wechat.yunniao.cn/'
@@ -27,12 +27,11 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     // Do something before request is sent
-    // let token = localStorage.getItem('token')
-    // if (token) {
-    //   // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
-    //   config.headers['Authorization'] = getToken()
-    //   config.headers['Authorization'] = 'eyJhbGciOiJIUzI1NiJ9.eyJwcm9maWxlIjoibTEiLCJ1c2VySWQiOiI4NyIsInVzZXJuYW1lIjoiMTUwMjE1Nzg1MDIiLCJ0eXBlIjoiMyIsImJ1c2lQZXJtaXNzaW9uIjoiIiwidXNlckxvZ2luRmxhZyI6IjBkYWI1NjAzNTE0YTQyNTNhZjIzODdmNzIwYjBmNDliIiwiZXhwIjoxNjAyMDM2MjE2fQ.SyvJCfGkkGhWwC04jthROAjNIR3gydVvjNSszCNF9OM'
-    // }
+    let token = localStorage.getItem('token')
+    if (token) {
+      // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
+      config.headers['Authorization'] = getToken()
+    }
     let cookieName = localStorage.getItem('developer')
     if (cookieName) {
       JsCookie.set('developer', cookieName);
@@ -75,7 +74,7 @@ service.interceptors.response.use(
       let unauthorityUrl = window.location.href;
       localStorage.setItem('unauthorityUrl', unauthorityUrl)
       // location.href = '/unauthority'
-      location.replace('/unauthority')
+      location.replace('/login')
       // 50008:非法的token; 50012:其他货主端登录了;  40101:Token 过期了;
       // if (res.code === 50008 || res.code === 50012 || res.code === 40101) {
       //   // to re-login
