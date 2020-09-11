4b7c819aedf4eea50c429afd630339f872bac8b4
zhaowei <zhaowei@yunniao.me>
2020-09-11 18:07:31 +0800

    修改bug

--- src/views/tryRun/TryRun.vue
+++ src/views/tryRun/TryRun.vue
@@ -470,14 +470,7 @@ export default {
     },
     // 获取列表
     async getLists(isInit) {
-      let toast;
       try {
-        toast = this.$toast.loading({
-          duration: 0,
-          message: '加载中...',
-          forbidClick: true,
-          loadingType: 'spinner'
-        })
         const params = this.delForm(this.form);
         params.page = this.page.current;
         params.limit = this.page.limit;
@@ -514,8 +507,6 @@ export default {
         this.refreshing = false;
         this.finished = true;
         console.log(`get list fail:${err}`);
-      } finally {
-        toast.clear();
       }
     }
   }
