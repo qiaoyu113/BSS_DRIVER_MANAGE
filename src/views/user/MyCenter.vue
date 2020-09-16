3497f42e7d578b351cc46ac45624553af9d50615
qiaoyu <qiaoyu@yunniao.me>
2020-09-16 12:23:18 +0800

    新增退出

--- src/views/user/MyCenter.vue
+++ src/views/user/MyCenter.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="mycenter">
+    <div style="padding:30px;box-sizing: border-box;">
+      <van-button style="margin-top:40%" color="linear-gradient(to right, #ff6034, #ee0a24)" block @click="removeLocal">
+        退出登陆
+      </van-button>
+    </div>
     <footer-tabbar />
   </div>
 </template>
@@ -24,7 +29,12 @@ export default {
   mounted() {
   },
   methods: {
-
+    removeLocal() {
+      localStorage.clear();
+      this.$router.replace({
+        name: 'login'
+      })
+    }
   }
 }
 </script>
