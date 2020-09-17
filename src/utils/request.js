ec352100cdf0391fc3e6a9169bf3883d5c2dfdbe
zhaowei <zhaowei@yunniao.me>
2020-09-17 17:48:59 +0800

    修改网络超时

--- src/utils/request.js
+++ src/utils/request.js
@@ -21,7 +21,7 @@ const service = axios.create({
 
   baseURL: process.env.VUE_APP_BASE_API, // url url = base url + request url
   withCredentials: true, // send cookies when cross-domain requests
-  timeout: 50000 // request timeout
+  timeout: 5000 // request timeout
 })
 
 // request interceptor
