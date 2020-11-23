33bab2e4d0aaabca5c4f92f55a993d09453b9758
zhaowei <zhaowei@yunniao.me>
2020-11-23 14:56:21 +0800

    修改打标签bug（赵维）

--- src/views/line/Tag/index.vue
+++ src/views/line/Tag/index.vue
@@ -211,7 +211,7 @@ export default {
             isHot: result.isHot,
             sellPoint: result.sellPoint
           }
-          this.checkedList = result.sellPoint.split(',').map(item => +item)
+          this.checkedList = result.sellPoint.split(',').filter(item => item).map(item => +item)
           this.checkedStrList = this.sellPointColumns.filter(item => this.checkedList.includes(item.value))
         } else {
           this.$fail(res.errorMsg)
